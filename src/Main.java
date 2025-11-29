00d6a6b449922143cd755af50d7ac69fea05afe7
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2025-11-29 23:39:32 +0530

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        int[] nums = new int[5];
+        nums[0] = 10;
+        nums[1] = 20;
+
+
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
 
     }
 }
