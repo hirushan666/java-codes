c40201911c6283a9d8a223a69bf920cf426b224f
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2025-11-30 00:16:19 +0530

    edited

---
+++ src/Inputfromuser.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Inputfromuser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i<arr.length; i++){
+            System.out.println("Enter element " + (i+1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
