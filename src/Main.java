22dc6336c8f0c0968864aca323646a7c2cfe04d6
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2025-11-29 23:39:56 +0530

    edit

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println(nums[0]);
         System.out.println(nums[1]);
         System.out.println(nums[2]);
+        System.out.println(nums[2]);
 
     }
 }
