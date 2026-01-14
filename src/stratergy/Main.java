0ff535ce95919075f5019eefb545a5093e1584dc
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2026-01-14 09:12:31 +0530

    Strategy Design Pattern

---
+++ src/stratergy/Main.java
@@ -0,0 +1,15 @@
+package stratergy;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.setPaymentStrategy(new CreditCardPayment());
+        cart.checkout(1000);
+
+        cart.setPaymentStrategy(new PayPalPayment());
+        cart.checkout(500);
+    }
+}
+
